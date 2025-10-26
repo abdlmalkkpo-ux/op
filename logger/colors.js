f9088c0396867a9ea3531e45c65c3e27cadb99d7
abdlmalkkpo-ux <abdlmalk.kpo@gmail.com>
2025-10-26 20:54:19 +0200

    Create colors.js

---
+++ logger/colors.js
@@ -0,0 +1,12 @@
+// src/logger/colors.js
+const colors = {
+  reset: "\x1b[0m",
+  bright: "\x1b[1m",
+  fgGreen: "\x1b[32m",
+  fgRed: "\x1b[31m",
+  fgYellow: "\x1b[33m",
+  fgCyan: "\x1b[36m",
+  fgMagenta: "\x1b[35m",
+};
+
+module.exports = colors;

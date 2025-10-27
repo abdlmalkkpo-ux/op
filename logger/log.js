949aa0c238bb0dc1ead520b6d4ac71e703bea095
abdlmalkkpo-ux <abdlmalk.kpo@gmail.com>
2025-10-27 14:31:10 +0200

    Update log.js

--- logger/log.js
+++ logger/log.js
@@ -1,7 +1,7 @@
 const { colors } = require('./colors.js');
 const moment = require("moment-timezone");
 const characters = '';
-const getCurrentTime = () => colors.gray(moment().tz("Africa/Algiers").format("HH:mm:ss DD/MM/YYYY"));
+//const getCurrentTime = () => colors.gray(moment().tz("Africa/Algiers").format("HH:mm:ss DD/MM/YYYY"));
 
 function logError(prefix, message) {
 	if (message === undefined) {
